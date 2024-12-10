a2aea77584cca6cd907a352bb75d7fb14e775ef1
haru <hinagoro5656@gmail.com>
2024-12-10 23:25:10 +0900

    baseを修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pomo-time/",
+  base: "/pomotime-v2/",
   plugins: [react()],
 });
